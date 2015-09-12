1f80c24cd638629f4d91076358ff1f417977a8a5
Cristian Vincenzini <borgesvive@hotmail.com>
2015-09-12 16:24:20 -0300

    Añade ejercicios 1 y 2 del tp número 4.

---
+++ src/TrabajoPracticoN4/Racional.java
@@ -0,0 +1,133 @@
+/*
+ * Implementar el TDA Racional respetando la definición dada en la guía.
+ */
+package TrabajoPracticoN4;
+
+/**
+ *
+ * @author Cristian
+ * @version 12/09/2015
+ */
+public class Racional {
+    private int numerador;
+    private int denominador;
+
+// Constructores
+    public Racional(int n, int d) {
+        numerador = n;
+        denominador = d;
+
+        if (denominador == 0) {
+            denominador = 1;
+        }
+    }
+
+// Observadores
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public double pasarADouble() {
+        return (double) numerador / denominador;
+    }
+
+    public String aCadena() {
+        return numerador + "/" + denominador;
+    }
+
+    public boolean igual(Racional r) {
+        return numerador == r.numerador && denominador == r.denominador;
+    }
+
+// Modificadores
+    public void setNumerador(int n) {
+        numerador = n;
+    }
+
+    public void setDenominador(int d) {
+        if (d > 0) {
+            denominador = d;
+        }
+    }
+
+// Propias del tipo
+    public Racional sumar(Racional r) {
+        Racional c;
+
+        c = new Racional(numerador + r.numerador, denominador + r.denominador);
+
+        return c;
+    }
+
+    public Racional restar(Racional r) {
+        int resto;
+        Racional c;
+
+        resto = denominador - r.denominador;
+        
+        if (resto == 0) {
+            c = null;
+        } else {
+            c = new Racional(numerador - r.numerador, resto);
+        }
+
+        return c;
+    }
+
+    public Racional multiplicar(Racional r) {
+        Racional c;
+
+        c = new Racional(numerador * r.numerador, denominador * r.denominador);
+
+        return c;
+    }
+
+    public Racional dividir(Racional r) {
+        Racional c = r;
+
+        if (r.denominador > 0) {
+            c = new Racional (numerador / r.numerador, denominador / r.denominador);
+        }
+
+        return c;
+    }
+
+    public void simplificar() {
+        int mcd, den;
+
+        mcd = mcd(numerador, denominador);
+        den = denominador / mcd;
+
+        if (den == 1) {
+            System.out.println(numerador / mcd);
+        } else {
+            System.out.println(numerador / mcd + "/" + den);
+        }
+    }
+
+    /**
+     * Calcula el máximo común divisor entre dos números utilizando
+     * el algoritmo de Euclides.
+     * 
+     * @param a numerador
+     * @param b denominador
+     * @return máximo común divisor entre a y b
+     */
+    private int mcd(int a, int b) {
+        int tmp;
+        
+        while (b != 0) {
+            tmp = b;
+            b = a % b;
+            
+            a = tmp;
+        }
+
+        return a;
+    }
+
+}
---
+++ src/TrabajoPracticoN4/testEspecie.java
@@ -0,0 +1,54 @@
+/*
+ * Implementar la clase testEspecie que permita probar la funcionalidad
+ * del TDA Especie. Es decir, cargar los datos de una o más especies, consultarlas,
+ * modificarlas y aplicar las operaciones propias del tipo.
+ *
+ * La especie rinoceronte tiene una población de 100 y una tasa de crecimiento de 15%
+ * y la especie elefante tiene una población de 10 y una tasa de crecimiento de 35%.
+ * ¿Cuántos años le llevará a la población elefante superar a la especie rinoceronte?
+ */
+package TrabajoPracticoN4;
+
+import Utiles.TecladoIn;
+
+/**
+ *
+ * @author Cristian
+ * @version 12/09/2015
+ */
+public class testEspecie {
+
+    public static void main(String[] args) {
+        int años, i;
+        boolean laSupera;
+
+        i        = 1;
+        laSupera = false;
+
+        Especie caballo     = new Especie("Caballo", 30, 20);
+        Especie rinoceronte = new Especie("Rinoceronte", 100, 15);
+        Especie elefante    = new Especie("Elefante", 10, 35);
+
+        System.out.println(caballo.aCadena());
+
+        if (!rinoceronte.igual(caballo)) {
+            System.out.println("Un " + caballo.getNombre() + " no es igual a un " + rinoceronte.getNombre());
+        }
+
+        System.out.print("Ingrese cantidad de años para estimar la población de caballos: ");
+        años = TecladoIn.readLineInt();
+        System.out.println("Población proyectada: " + caballo.poblacionProyectada(años) + "\n");
+
+        while(!laSupera && i < 200) { // para evitar un posible ciclo infinito
+            if (elefante.poblacionProyectada(i) > rinoceronte.poblacionProyectada(i)) {
+                laSupera = true;
+            } else {
+                i++;
+            }
+        }
+
+        System.out.println("La población de elefantes superará a la de los rinocerontes en " + i + " años.");
+        System.out.println("Población de elefantes en " + i + " años: " + elefante.poblacionProyectada(i));
+        System.out.println("Población de rinocerontes en " + i + " años: " + rinoceronte.poblacionProyectada(i));
+    }
+}
