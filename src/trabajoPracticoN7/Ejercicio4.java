61adc429ed764e45ac5598c696792d8b2102a0ed
Cristian Vincenzini <borgesvive@hotmail.com>
2015-10-31 14:26:28 -0300

    Finaliza ejercicio 4, añade ejercicio 5

--- src/trabajoPracticoN7/Ejercicio4.java
+++ src/trabajoPracticoN7/Ejercicio4.java
@@ -19,33 +19,49 @@ public class Ejercicio4 {
 
     /**
      * Busca recursivamente un elemento dentro de un arreglo desordenado.
-     * 
-     * @param a arreglo de entrada
-     * @param indice posición desde la cual se comienza a buscar
-     * @param elemento entero a buscar
-     * @return verdadero si el elemento está en el arreglo
+     *
+     * @param a arreglo desordenado
+     * @param e elemento a buscar
+     * @param p tamaño del arreglo menos uno
+     * @return verdadero si el elemento se encuentra en el arreglo
      */
-    public static boolean tieneElemento(int[] a, int indice, int elemento) {
+    public static boolean tieneElemento(int[] a, int e, int p) {
         boolean encontrado;
 
-        encontrado = false;
-
-        if (a[indice] == elemento) {
-            encontrado = true;
+        if (p == 0) {
+            encontrado = (a[0] == e);
         } else {
-            if (indice < a.length - 1) {
-                encontrado = tieneElemento(a, indice + 1, elemento);
-            } else {
-                encontrado = false;
-            }
+            encontrado = (e == a[p]) || (tieneElemento(a, e, p - 1));
         }
 
         return encontrado;
     }
 
+    public static boolean esCreciente(int[] a, int p) {
+        boolean esCreciente;
+        
+        if (p > 0) {
+            if (a[p] < a[p - 1]) {
+                esCreciente = false;
+            } else {
+                esCreciente = (p == 1) || esCreciente(a, p - 1);
+            }
+        } else {
+            esCreciente = true;
+        }
+
+        return esCreciente;
+    }
+
     public static void main(String[] args) {
         int[] v = {12, 7, 0, -1, 8, 10};
 
-        System.out.println(tieneElemento(v, 0, 12));
+        System.out.println(tieneElemento(v, 10, v.length - 1));
+
+        if (esCreciente(v, v.length - 1)) {
+            System.out.println("El arreglo es creciente.");
+        } else {
+            System.out.println("El arreglo no respeta un orden creciente.");
+        }
     }
 }
---
+++ src/trabajoPracticoN7/Ejercicio5.java
@@ -0,0 +1,19 @@
+/*
+ * Dada una secuencia S de números enteros positivos (finalizada en 0, que no se
+ * considera parte de la secuencia) ingresados por teclado, escribir planteos
+ * recursivos, sus correspondientes métodos, y realizar la traza para la secuencia
+ * 2 5 3 6 12 3 0 en los siguientes incisos:
+ *
+ * a) Calcular cuántos enteros múltiplos de 4 hay en la secuencia.
+ * b) Determinar el máximo entero perteneciente a la secuencia.
+ */
+package trabajoPracticoN7;
+
+/**
+ *
+ * @author Cristian
+ * @version 31/10/2015
+ */
+public class Ejercicio5 {
+    // TODO
+}
