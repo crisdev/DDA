03c82622db0bfead041f361aa059918d9050df42
Cristian Vincenzini <borgesvive@hotmail.com>
2015-10-31 22:24:22 -0300

    Ejercicio 5a, b sin terminar

--- src/trabajoPracticoN7/Ejercicio5.java
+++ src/trabajoPracticoN7/Ejercicio5.java
@@ -9,11 +9,45 @@
  */
 package trabajoPracticoN7;
 
+import utiles.TecladoIn;
+
 /**
  *
  * @author Cristian
  * @version 31/10/2015
  */
 public class Ejercicio5 {
-    // TODO
+
+    /**
+     * Calcula recursivamente la cantidad de mútiplos de 4 de una entrada de
+     * números.
+     *
+     * @return múltiplos de 4
+     */
+    public static int multiplosDeCuatro() {
+        int cantidad, n;
+
+        System.out.print("Ingrese un número: ");
+        n = TecladoIn.readLineInt();
+
+        if (n <= 0) {
+            cantidad = 0;
+        } else {
+            if (n % 4 == 0) {
+                cantidad = 1 + multiplosDeCuatro();
+            } else {
+                cantidad = multiplosDeCuatro();
+            }
+        }
+
+        return cantidad;
+    }
+
+    public static int mayorValor() {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Cantidad de múltiplos de 4: " + multiplosDeCuatro());
+    }
 }
