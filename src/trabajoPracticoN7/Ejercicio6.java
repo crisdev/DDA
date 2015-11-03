e18ecd9a484c2c51d129eb572c8bfca93123d71c
Cristian <borgesvive@hotmail.com>
2015-11-03 00:09:36 -0300

    Ejercicio 6

--- src/trabajoPracticoN7/Ejercicio6.java
+++ src/trabajoPracticoN7/Ejercicio6.java
@@ -14,35 +14,63 @@ import utiles.TecladoIn;
 /**
  *
  * @author Cristian
- * @version 31/10/2015
+ * @version 2/11/2015
  */
 public class Ejercicio6 {
-    
-    public static char imprimir(char c) {
+
+    /**
+     * Imprime una secuencia de caracteres excluyendo el parámetro ingresado.
+     * 
+     * @param c caracter a excluir en la representación
+     */
+    public static void imprimir(char c) {
         char e;
         
-        System.out.print("Ingrese un caracter (. para terminar): ");
+        System.out.print("Ingrese un caracter o . para terminar: ");
+        e = TecladoIn.readLineNonwhiteChar();
+        
+        if (e == '.') {
+            e = '\0';
+        } else {
+            if (e == c) {
+                e = '\0';
+            }
+            imprimir(c);
+        }
+
+        System.out.print(e);
+    }
+    
+    public static int cuentaCaracter(char a) {
+        int contador = 0;
+        char e;
+        
+        System.out.print("Ingrese un caracter o . para terminar: ");
         e = TecladoIn.readLineNonwhiteChar();
         
         if (e != '.') {
-            if (c != e) {
-                System.out.print(imprimir(e));
+            if (e == a) {
+                contador++;
+                cuentaCaracter(a);
             } else {
-                System.out.println("hola");
+                cuentaCaracter(a);
             }
-        } else {
-            e  = '\0';
         }
         
-        return e;
+        return contador;
     }
-    
+
     public static void main(String[] args) {
         char caracter;
+
+        //System.out.print("Ingrese un caracter a excluir: ");
+        //caracter = TecladoIn.readLineNonwhiteChar();
+
+        //imprimir(caracter);
         
-        System.out.print("Ingrese un caracter a excluir: ");
+        System.out.print("Ingrese un caracter a contar: ");
         caracter = TecladoIn.readLineNonwhiteChar();
         
-        imprimir(caracter);
+        System.out.println("Repeticiones: " + cuentaCaracter(caracter));
     }
 }
